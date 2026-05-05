03d32f3ab42529c82e99fa930cad11d1ba757e29
PedroGonçalvesBrito <pedrogoncalvesbrito0@gmail.com>
2026-05-04 21:29:43 -0400

    h

--- app/helpers/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-define('ROOT', dirname(__FILE__, 3));
-#DIRETÓRIO DAS VIEWS
-define('DIR_VIEWS', ROOT . '/app/view');
-#EXTENSÃO PADRÃO DAS VIEWS
-define('EXT_VIEWS', '.html');
