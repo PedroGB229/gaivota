9674894ad2f1e47bc09930a7f73ec20c74275567
PedroGonçalvesBrito <pedrogoncalvesbrito0@gmail.com>
2026-06-03 20:45:43 -0400

    finalmente

--- resources/js/components/Chart.js
+++ resources/js/components/Chart.js
@@ -1,21 +1,16 @@
-// Classe que constrói e renderiza gráficos usando ECharts
 class Chart {
   setId(id)    { this._id   = id;    return this; }
   getData(url) { this._url  = url;   return this; }
   BAR()        { this._type = 'bar'; return this; }
   PIE()        { this._type = 'pie'; return this; }
 
-
-  // busca os dados da API e renderiza o gráfico
   async render() {
     const data  = await fetch(this._url).then(r => r.json());
-    const chart = window.echarts.init(document.getElementById(this._id)); // usa window.echarts
+    const chart = window.echarts.init(document.getElementById(this._id));
     chart.setOption(this._type === 'pie' ? buildPie(data) : buildBar(data));
   }
 }
 
-// monta a opção do ECharts para gráfico de barras
-// a API deve retornar: { categories: ['Jan','Fev',...], values: [10, 20, ...] }
 function buildBar(data) {
   return {
     tooltip: {},
@@ -25,8 +20,6 @@ function buildBar(data) {
   };
 }
 
-// monta a opção do ECharts para gráfico de pizza
-// a API deve retornar: { series: [{ name: 'X', value: 100 }, ...] }
 function buildPie(data) {
   return {
     tooltip: { trigger: 'item' },
@@ -34,5 +27,9 @@ function buildPie(data) {
   };
 }
 
-// exporta uma única instância reutilizável
-export default Chart;
+// Proxy que cria uma nova instância a cada chamada de método estático
+export default new Proxy(Chart, {
+  get(target, prop) {
+    return (...args) => new target()[prop](...args);
+  }
+});
